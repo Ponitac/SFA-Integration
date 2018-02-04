6f367f420414a2929e6a25027c4a666220244612
Ponitac <uholtgrave@googlemail.com>
2018-02-04 13:41:41 +0100

    Integrating Redirect into Plugin

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  sfa-TalentLMS-Integration
 Description:  This plugin integrates the TalentLMS API Library and enables WordPress to automatically register and login WordPress users on TalentLMS
-Version:      0.9
+Version:      1.0
 Author:       Matteo Ramin, Jan-Ulrich Holtgrave
 Author URI:   https://github.com/ponitac
 License:      GPL2
@@ -43,13 +43,14 @@ function registerUserOnTLMS($user_login, $user){
 }
 add_action('wp_login', 'registerUserOnTLMS', 10, 2);
 
+
 /**
  * Initiates the plugin, 
  * i.e. sets up the database extension and sets API key and TLMS domain
  */
 function init(){
     header('Content-Type: text/html; charset=utf-8');
-    initDatabase(); // Create database if not already existant
+    initDatabase(); // Create database if not already existing
     
     try{
         //Initiate API
@@ -60,11 +61,63 @@ function init(){
         echo $e->getMessage();
     }    
 }
-
+//Update Profile when changes appeared
 function onProfileUpdate( $user_id, $old_user_data ) {
     prepareProfileUpdate($user_id, $old_user_data);
 }
 add_action( 'profile_update', 'onProfileUpdate', 10, 2 );
+
+function redirect_via_customized_post(){
+    if ($post->ID == getRedirectPostID){
+        try{
+            header("Cache-Control: no-cache, must-revalidate");
+            header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        
+            //Initiate API    
+            TalentLMS::setApiKey(getAPIKEY()); // Set the API key according to the options
+            TalentLMS::setDomain(getDomain()); // Set the domain accoding to the options
+        
+            $userEmail = wp_get_current_user()->user_email;
+            $userPassword = getTLMSPasswordByMail($userEmail);
+            $logoutRedirect = get_home_url();
+        
+            $returnSet = TalentLMS_User::login(array(
+                'login' => $userEmail,
+                'password' => $userPassword, 
+                'logout_redirect' => $logoutRedirect)
+            );
+        
+            // 
+            
+            // Retrieve login key from return JSON
+            $loginKey = $returnSet['login_key'];
+            header("Location: ".$loginKey );
+            exit;
+        
+        } catch (Exception $e){
+        
+            try{
+                $userEmail = wp_get_current_user()->user_email;
+                $returnSet = TalentLMS_User::retrieve(array('email' => $userEmail));
+        
+                // Retrieve login key from return JSON
+                $loginKey = $returnSet['login_key'];
+                header("Location: ".$loginKey );
+                exit;
+            } catch (Exception $e2){
+                error_log($e->getMessage());
+                error_log($e->getHttpStatus());
+                error_log($e2->getMessage());
+                error_log($e2->getHttpStatus());
+                echo "You need to be signed in to Social Finance Academy in order to access the online courses of Social Finance Academy. Please go back and log in.";
+            }
+    }
+}
+
+
+
+
+add_filter( 'post_link', 'redirect_via_customized_post', 10, 3 );
 
 
 /**
